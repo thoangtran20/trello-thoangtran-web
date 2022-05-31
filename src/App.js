3c839b11a0e17f008f739d240f1bcf9977df905d
thoangtran20 <77189736+thoangtran20@users.noreply.github.com>
2022-05-31 17:19:32 +0700

    Design trello Front-end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,198 @@
-import './App.css'
+import './App.scss'
 
 function App() {
-    return ( 
-        <div className = "App">
-            <h1 className='hello-world'>Hello World</h1>
+  return (
+    <div className="trello-thoangtran-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <nav className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
         </div>
-    );
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://www.mindinventory.com/blog/wp-content/uploads/2021/06/MEAN-vs-MERN.png"
+                alt="mernstack-alt-img"
+              />
+              Title: MERN STACK
+            </li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+            <li>Add you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </nav>
+    </div>
+  )
 }
 
 export default App
