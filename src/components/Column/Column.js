743864925d02cfc979d2396b34d3e05a0eaf2028
thoangtran20 <77189736+thoangtran20@users.noreply.github.com>
2022-06-01 16:58:25 +0700

    Clean React Architecture

---
+++ src/components/Column/Column.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './Column.scss'
+import Task from 'components/Task/Task'
+
+function Column() {
+  return (
+    <div className="column">
+      <header>Brainstorm</header>
+      <ul className="task-list">
+        <Task />
+        <li className="task-item">Add you'd like to work on below</li>
+        <li className="task-item">Add you'd like to work on below</li>
+        <li className="task-item">Add you'd like to work on below</li>
+        <li className="task-item">Add you'd like to work on below</li>
+        <li className="task-item">Add you'd like to work on below</li>
+        <li className="task-item">Add you'd like to work on below</li>
+       
+      </ul>
+      <footer>Add another card</footer>
+    </div>
+  )
+}
+
+export default Column
